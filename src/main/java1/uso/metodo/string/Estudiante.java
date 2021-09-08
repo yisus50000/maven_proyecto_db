d7e2b74c4171d8e31d4f4c3e868bc83fef18c59d
Diego Benitez <ioritene555@gmail.com>
2021-09-08 17:30:45 -0500

    Correccion

--- src/main/java1/uso/metodo/string/Estudiante.java
+++ src/main/java1/uso/metodo/string/Estudiante.java
@@ -2,7 +2,6 @@ package uso.metodo.string;
 
 public class Estudiante extends MainEstudiante {
 
-	public String cedulaGuardada;
 	public String cedula;
 
 	public String getCedula() {
@@ -11,13 +10,5 @@ public class Estudiante extends MainEstudiante {
 
 	public void setCedula(String cedula) {
 		this.cedula = cedula;
-	}
-
-	public String getCedulaGuardada() {
-		return cedulaGuardada;
-	}
-
-	public void setCedulaGuardada(String cedulaGuardada) {
-		this.cedulaGuardada = cedulaGuardada;
 	}
 }
--- src/main/java1/uso/metodo/string/MainEstudiante.java
+++ src/main/java1/uso/metodo/string/MainEstudiante.java
@@ -8,23 +8,25 @@ public class MainEstudiante {
 		// TODO Auto-generated method stu
 		Scanner teclado = new Scanner(System.in);
 		int selec;
-		String cedula;
+		String cedula = "";
+
+		Estudiante lista[] = new Estudiante[5];
 		Estudiante cedula1 = new Estudiante();
-		cedula1.setCedulaGuardada(cedula);
+		cedula1.setCedula(cedula);
 		Estudiante cedula2 = new Estudiante();
-		cedula2.setCedulaGuardada(cedula);
+		cedula2.setCedula(cedula);
 		Estudiante cedula3 = new Estudiante();
-		cedula3.setCedulaGuardada(cedula);
+		cedula3.setCedula(cedula);
 		Estudiante cedula4 = new Estudiante();
-		cedula4.setCedulaGuardada(cedula);
+		cedula4.setCedula(cedula);
 		Estudiante cedula5 = new Estudiante();
-		cedula5.setCedulaGuardada(cedula);
-		Estudiante estudiante[] = new Estudiante[5];
-		estudiante[0] = cedula1;
-		estudiante[1] = cedula2;
-		estudiante[2] = cedula3;
-		estudiante[3] = cedula4;
-		estudiante[4] = cedula5;
+		cedula5.setCedula(cedula);
+		lista[0] = cedula1;
+		lista[1] = cedula2;
+		lista[2] = cedula3;
+		lista[3] = cedula4;
+		lista[4] = cedula5;
+
 		do {
 			System.out.println("*********************");
 			System.out.println("*****ELIJA UNA OPCION*****");
@@ -33,30 +35,39 @@ public class MainEstudiante {
 			System.out.println("3. SALIR");
 			System.out.println("*********************");
 			selec = teclado.nextInt();
-			cedula = teclado.nextLine();
-
 			if (selec == 1) {
 				System.out.println("Ingrese cedula del estudiante: ");
 				cedula = teclado.nextLine();
-				for (int i = 0; i < 5; i++) {
-					estudiante estudiantec = estudiante[1];
-					String cedula = estudiante.getCedula;
+				for (int i = 0; i == 0; i++) {
+					lista[i] = cedula1;
+				}
+				for (int i = 0; i == 1; i++) {
+					lista[i] = cedula2;
+				}
+				for (int i = 0; i == 2; i++) {
+					lista[i] = cedula3;
+				}
+				for (int i = 0; i == 3; i++) {
+					lista[i] = cedula4;
+				}
+				for (int i = 0; i == 4; i++) {
+					lista[i] = cedula5;
 				}
 			} else if (selec == 2) {
 				System.out.println("Ingrese cedula del estudiante: ");
-				String cadena1 = cedula;
-				String cadena2 = teclado.nextLine();
-				boolean resultado = cadena1.equals(cadena2);
-				System.out.println("La cedula es: " + resultado);
-
-			} else if (selec == 3) {
-				System.out.println("Hasta Pronto");
-				selec = teclado.nextInt();
+				cedula = teclado.nextLine();
+				for (int i = 0; i < 5; i++) {
+					Estudiante estudiante = lista[1];
+					String cedulaObt = estudiante.getCedula();
+					if (cedula.equals(cedulaObt)) {
+						System.out.println("El estudiante a sido registrado");
+					} else {
+						System.out.println("El estudiante no ha sido registrado");
+					}
+				}
 			}
-
-		} while (selec == 3);
+		} while (selec != 3);
 		selec = teclado.nextInt();
-
 	}
 
 }
